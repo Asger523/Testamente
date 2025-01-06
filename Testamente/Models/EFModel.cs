27b6b4b36f95eb7fca97406f1a09f7801b241ded
Greyrib <mlabildgaard@live.dk>
2025-01-06 10:10:31 +0100

    Relatively simple example for live coding exam

--- Testamente/Models/EFModel.cs
+++ Testamente/Models/EFModel.cs
@@ -1,7 +1,15 @@
-﻿namespace Testamente.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Testamente.Models
 {
     public class EFModel
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "HEY! DU HAR GLEMT NAME!!! >:(")]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
     }
 }
